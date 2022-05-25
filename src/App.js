2712e9f185bedab11f8de65e2ed68212835dade6
Tsuyoshi Shiojiri <shiojiri@shiojiri-macbook-air.local>
2022-05-26 08:00:09 +0900

    use props

--- src/App.js
+++ src/App.js
@@ -1,10 +1,28 @@
-import {Component} from "react";
+import React from "react";
 
-class App extends Component {
-  render() {
-    return <div className="App"><h1>Hello, world!</h1></div>;
-  }
+const App = () => {
+    const profiles = [
+        { name: "Taro", age: 10},
+        { name: "Hanako", age: 5},
+        { name: "NoName"},
+    ]
+    return(
+        <div>
+        {
+            profiles.map((profile, index) => {
+                return <User name={profile.name} age={profile.age} key={index} />;
+            })
+        }
+        </div>
+    )
+}
 
+const User = (props) => {
+    return <div>Hi! I am {props.name}, and {props.age} years old!</div>
+}
+
+User.defaultProps = {
+    age: 1
 }
 
 export default App;
