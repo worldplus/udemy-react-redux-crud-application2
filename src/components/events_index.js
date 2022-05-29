c39ce74eaf7107e3d43826b376749fc182490176
Tsuyoshi Shiojiri <shiojiri@shiojiri-macbook-air.local>
2022-05-29 17:06:55 +0900

    implement events index

---
+++ src/components/events_index.js
@@ -0,0 +1,44 @@
+import React, {Component} from "react";
+import { connect } from "react-redux";
+import _ from 'lodash';
+
+import { readEvents } from "../actions";
+
+class EventsIndex extends Component {
+    componentDidMount() {
+        this.props.readEvents()
+    }
+
+    renderEvents() {
+        return _.map(this.props.events, event => (
+            <tr key={event.id}>
+                <td>{ event.id }</td>
+                <td>{ event.title }</td>
+                <td>{ event.body }</td>
+            </tr>
+        ))
+    }
+
+    render() {
+        return (
+            <table>
+                <tr>
+                    <th>ID</th>
+                    <th>Title</th>
+                    <th>Body</th>
+                </tr>
+                <tbody>{ this.renderEvents() }</tbody>
+            </table>
+        )
+    }
+}
+
+const mapStateToProps = state => ({ events: state.events })
+
+// const mapDispatchToProps = dispatch =>({
+//     increment: () => dispatch(increment()),
+//     decrement: () => dispatch(decrement()),
+// })
+const mapDispatchToProps = ({ readEvents })
+
+export default connect(mapStateToProps, mapDispatchToProps)(EventsIndex)
