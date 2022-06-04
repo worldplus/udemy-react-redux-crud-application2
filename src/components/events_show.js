a4aa3c0be2cf865c9ac180147df196dd44097ef2
Tsuyoshi Shiojiri <shiojiri@shiojiri-macbook-air.local>
2022-06-04 16:17:31 +0900

    implement eventsShow

--- src/components/events_show.js
+++ src/components/events_show.js
@@ -12,8 +12,13 @@ class EventsShow extends Component {
         this.onDeleteClick = this.onDeleteClick.bind(this)
     }
 
+    componentDidMount() {
+        const { id } = this.props.match.params
+        if (id) this.props.getEvent(id)
+    }
+
     renderField(field) {
-        const {input, label, type, meta: { touched, error } } = field
+        const { input, label, type, meta: { touched, error } } = field
 
         return (
             <div>
@@ -30,25 +35,23 @@ class EventsShow extends Component {
     }
 
     async onSubmit(values) {
-        await this.props.postEvent(values)
+        await this.props.putEvent(values)
         this.props.history.push('/')
     }
 
     render() {
-        const { handleSubmit, pristine, submitting } = this.props;
+        const { handleSubmit, pristine, submitting, invalid } = this.props;
         return (
-            <React.Fragment>
-                <form onSubmit={handleSubmit(this.onSubmit)}>
-                    <div><Field label="Title" name="title" type="text" component={this.renderField} /></div>
-                    <div><Field label="Body" name="body" type="text" component={this.renderField} /></div>
+            <form onSubmit={handleSubmit(this.onSubmit)}>
+                <div><Field label="Title" name="title" type="text" component={this.renderField} /></div>
+                <div><Field label="Body" name="body" type="text" component={this.renderField} /></div>
 
-                    <div>
-                        <input type="submit" value="Submit" disabled={pristine || submitting}/>
-                        <Link to="/">Cancel</Link>
-                        <Link to="/" onClick={this.onDeleteClick}>Delete</Link>
-                    </div>
-                </form>
-            </React.Fragment>
+                <div>
+                    <input type="submit" value="Submit" disabled={pristine || submitting || invalid}/>
+                    <Link to="/">Cancel</Link>
+                    <Link to="/" onClick={this.onDeleteClick}>Delete</Link>
+                </div>
+            </form>
         )
     }
 }
@@ -61,8 +64,14 @@ const validate = values => {
 
     return errors
 }
-const mapDispatchToProps = ({ deleteEvent })
 
-export default connect(null, mapDispatchToProps)(
-    reduxForm({ validate, form: 'eventShowForm' })(EventsShow)
+const mapStateToProps = (state, ownProps) => {
+    const event = state.events[ownProps.match.params.id]
+    return { initialValues: event, state }
+}
+
+const mapDispatchToProps = ({ deleteEvent, getEvent, putEvent })
+
+export default connect(mapStateToProps, mapDispatchToProps)(
+    reduxForm({ validate, form: 'eventShowForm', enableReinitialize: true })(EventsShow)
 )
